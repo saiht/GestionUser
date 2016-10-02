778f7a5d97e7f96c7a711578d82f249465085e1e
Mathias CADET <mattttik@MacBook-Pro-de-Mathias.local>
2016-10-02 13:51:10 +0200

    système d authentification avec confirmation par mail : ok

---
+++ app/Notifications/RegisteredUser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class RegisteredUser extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Inscription sur mon super site ^^')
+                    ->line('Votre compte doit être confirmer.')
+                    ->action('Confirmer mon adresse email', url("confirmation/{$notifiable->id}/{$notifiable->confirmation_token}"))
+                    ->line('Si vous n\'êtes pas à l\'origine de cette demande, vous pouvez ignorer cet email.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
